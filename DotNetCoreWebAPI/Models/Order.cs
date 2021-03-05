c45c9f7e4732c1d9baf2087d7a2c33e7f85c640d
mathews225 <petemathews14@gmail.com>
2021-03-05 11:09:30 -0500

    Salesperson Class

--- DotNetCoreWebAPI/Models/Order.cs
+++ DotNetCoreWebAPI/Models/Order.cs
@@ -8,16 +8,33 @@ using System.Threading.Tasks;
 namespace DotNetCoreWebAPI.Models {
 	public class Order {
 		public int Id { get; set; }
+		
+		
 		[StringLength(200), Required]
 		public string Description { get; set; }
+		
+		
 		[StringLength(12), Required]
 		public string Status { get; set; }
+		
+		
 		[Column(TypeName = "decimal(9,2)")]
 		public decimal Total { get; set; }
+		
+		
 		public int CustomerId { get; set; }
+		
 		public virtual Customer Customer { get; set; } // Set customer as foreign key // Virtual = not in DB only in class
+
+
+		public int SalespersonId { get; set; }
+
+		public virtual Salesperson Salesperson { get; set; } // Set customer as foreign key // Virtual = not in DB only in class
+
+
+
 		[Required]
-		public virtual IEnumerable<Orderline> Orderlines { get; set; } // Virtual = not in DB only in class
+		public virtual IEnumerable<Orderline> Orderlines { get; set; } // Virtual = not in DB only in class. and to link the OrderController back to the orderline?
 
 
 		public Order() {}
---
+++ DotNetCoreWebAPI/Models/Salesperson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreWebAPI.Models {
+	public class Salesperson {
+
+		public int Id { get; set; } // Primary Key
+
+		
+		[StringLength(25), Required]
+		public string Name { get; set; }
+
+		
+		[StringLength(2), Required] // code max length 2, and make it required
+		public string StateCode { get; set; } // Must be unique
+		
+		[Column(TypeName = "decimal(11,2)")]
+		public decimal Sales { get; set; }
+
+	}
+}
