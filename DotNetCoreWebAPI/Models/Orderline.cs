af1cee0fa0e9d971abfe40b4940516c4f5befefe
mathews225 <petemathews14@gmail.com>
2021-03-05 10:56:33 -0500

    notes

--- DotNetCoreWebAPI/Models/Orderline.cs
+++ DotNetCoreWebAPI/Models/Orderline.cs
@@ -12,12 +12,12 @@ namespace DotNetCoreWebAPI.Models {
 		public int Id { get; set; }
 		[Required]
 		public int OrderId { get; set; }
-		[JsonIgnore]
+		[JsonIgnore] // required to use the (ThenInclue for item in order controller
 		public virtual Order Order { get; set; } // Virtual = not in DB only in class
 		[Required]
 		public int ItemId { get; set; }
 		public virtual Item Item { get; set; }
-		[Required]
+		[Required] // required for any required field that doesn't have default
 		public int Quantity { get; set; }
 
 	}
